95cbb39dce9817cfcdc6a3e7ac60cc269b387807
Anshu raj <anshur0202@gmail.com>
2021-11-14 12:49:44 +0530

    my first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PGLife
+It's a website prototype for online pg finder for student and working bachelors.
---
+++ includes/database_connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername	='localhost';
+$username = 'root';
+$password = '';
+$database='pglife';
+
+$conn = mysqli_connect($servername,$username,$password,$database);
+
+if (mysqli_connect_errno()) {
+    // Throw error message based on ajax or not
+    echo "Failed to connect to MySQL! Please contact the admin.";
+    return;
+}
